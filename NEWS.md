142953679829b87c1d12326c3760d9eefc1f03cc
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2026-02-16 14:18:24 +0000

    add new WS example

--- NEWS.md
+++ NEWS.md
@@ -12,6 +12,7 @@ to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 * New function to make a ws crunchable
 * Shiny app to compare WS with button to export table
+* New WS example and code to generate it
 
 ### Changed
 
---
+++ data-raw/ws_example.R
@@ -0,0 +1,67 @@
+## code to prepare `ws_example` dataset goes here
+
+
+library("rjd3toolkit")
+library("rjd3x13")
+library("rjd3workspace")
+
+set.seed(2025)
+
+context_FR <- create_insee_context()
+
+jws <- jws_new(modelling_context = context_FR)
+jsap <- jws_sap_new(jws, name = "ABS")
+
+for (id_series in seq_len(ncol(ABS))) {
+
+    # Outliers - domain
+    nb_out <- sample(10, size = 1)
+    out_date <- paste(
+        sample(1983:2017, size = nb_out, replace = TRUE),
+        sample(sprintf("%02d", 1:12), size = nb_out, replace = TRUE),
+        "01", sep = "-"
+    )
+    out_type <- sample(c("AO", "TC", "LS"), size = nb_out, replace = TRUE)
+
+    dspec <- x13_spec("RSA3") |>
+        add_outlier(type = out_type, date = out_date)
+
+    td <- sample(c("TradingDays", "WorkingDays", "TD2c", "TD3", "TD3c", "TD4", "None", "UserDefined", "Stock"),
+                 size = 1)
+
+    if (td == "Stock") {
+        dspec <- dspec |>
+            set_tradingdays(stocktd = sample(1:31, size = 1))
+    } else if (td == "UserDefined") {
+        set <- sample(names(context_FR$variables), size = 1)
+        dspec <- dspec |>
+            set_tradingdays(
+                option = "UserDefined",
+                uservariable = paste0(set, ".", names(context_FR$variables[[set]])),
+                test = "None"
+            )
+    } else {
+        dspec <- dspec |>
+            set_tradingdays(
+                option = td,
+                test = "None"
+            )
+    }
+
+    add_sa_item(jsap, name = colnames(ABS)[id_series], x = ABS[, id_series], spec = dspec)
+
+    # Outliers - estimation
+    nb_out <- sample(10, size = 1)
+    out_date <- paste(
+        sample(1983:2017, size = nb_out, replace = TRUE),
+        sample(sprintf("%02d", 1:12), size = nb_out, replace = TRUE),
+        "01", sep = "-"
+    )
+    out_type <- sample(c("AO", "TC", "LS"), size = nb_out, replace = TRUE)
+
+    espec <- dspec |>
+        add_outlier(type = out_type, date = out_date)
+    set_specification(jsap, idx = id_series, spec = espec)
+}
+
+save_workspace(jws, file = "inst/workspaces/ws_example.xml")
