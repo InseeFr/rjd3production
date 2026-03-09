f5f47be366efe48018b2d6357f44100c3232ffc4
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2026-03-09 11:30:11 +0000

    add pkg level doc

---
+++ R/rjd3production-package.R
@@ -0,0 +1,11 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom rjd3workspace read_sai
+## usethis namespace: end
+NULL
+
+.onLoad <- function(libname, pkgname) {
+    print("loaded")
+}
