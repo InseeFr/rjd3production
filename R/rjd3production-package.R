a05c921ee6ed4b0fdac0d4ef9add23f30c1212d1
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2026-03-09 11:35:40 +0000

    add requireNamespace

--- R/rjd3production-package.R
+++ R/rjd3production-package.R
@@ -9,5 +9,8 @@ NULL
 
 .onLoad <- function(libname, pkgname) {
     print("loaded")
+    if (! requireNamespace('rjd3toolkit', quietly = TRUE)) stop("Loading rjd3 libraries failed")
+    if (! requireNamespace("rjd3tramoseats", quietly = TRUE)) stop("Loading rjd3 libraries failed")
+    if (! requireNamespace("rjd3x13", quietly = TRUE)) stop("Loading rjd3 libraries failed")
     rjd3toolkit::reload_dictionaries()
 }
