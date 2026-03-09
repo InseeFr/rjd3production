e36c938de695e932671c798d45731cbfad4f4241
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2026-03-09 13:24:42 +0000

    add .jpackage

--- R/rjd3production-package.R
+++ R/rjd3production-package.R
@@ -5,13 +5,14 @@
 #' @import rjd3toolkit
 #' @import rJava
 #' @importFrom rjd3workspace read_sai
-#' @importFrom rJava .jinit .jaddClassPath
+#' @importFrom rJava .jinit .jaddClassPath .jpackage
 ## usethis namespace: end
 NULL
 
 .onLoad <- function(libname, pkgname) {
     print("loaded")
-
+    rJava::.jpackage(pkgname, lib.loc=libname)
+    print("after")
 
     # if (! requireNamespace('rjd3toolkit', quietly = TRUE)) stop("Loading rjd3 libraries failed")
     # if (! requireNamespace("rjd3tramoseats", quietly = TRUE)) stop("Loading rjd3 libraries failed")
