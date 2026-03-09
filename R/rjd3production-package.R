18129ed0de60d95ccad79b195c493ad497a25c1c
Tanguy BARTHELEMY <tanguy.barthelemy@insee.fr>
2026-03-09 11:40:18 +0000

    add rJava::.jinit()

--- R/rjd3production-package.R
+++ R/rjd3production-package.R
@@ -4,11 +4,15 @@
 ## usethis namespace: start
 #' @import rjd3toolkit
 #' @importFrom rjd3workspace read_sai
+#' @importFrom rJava .jinit
 ## usethis namespace: end
 NULL
 
 .onLoad <- function(libname, pkgname) {
     print("loaded")
+
+    rJava::.jinit()
+
     if (! requireNamespace('rjd3toolkit', quietly = TRUE)) stop("Loading rjd3 libraries failed")
     if (! requireNamespace("rjd3tramoseats", quietly = TRUE)) stop("Loading rjd3 libraries failed")
     if (! requireNamespace("rjd3x13", quietly = TRUE)) stop("Loading rjd3 libraries failed")
